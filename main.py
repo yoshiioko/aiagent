aca9d57c53395d3ac538f6f8be30c6ada04f2d3d
yoshiioko <acardoza@pdx.edu>
2025-12-15 18:18:59 -0800

    Refactor to use main function. Allow passing prompt through sys args. Update model used.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents="Why is the sky blue?"
-)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-print(response.text)
+    if len(sys.argv) < 2:
+        print('Usage: uv main.py "Your prompt message"')
+        sys.exit(1)
+
+    user_prompt = sys.argv[1]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=user_prompt
+    )
+
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
