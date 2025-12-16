b1fd327223883a92fcfcd7b071da91774299aaa6
yoshiioko <acardoza@pdx.edu>
2025-12-16 12:36:14 -0800

    Implement function that will allow agent to traverse file system.

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+
+def get_files_info(working_directory, directory=None):
+    abs_working_dir = os.path.abspath(working_directory)
+
+    if directory is None:
+        abs_directory = os.path.abspath(working_directory)
+    else:
+        abs_directory = os.path.abspath(os.path.join(working_directory, directory))
+
+    if not abs_directory.startswith(abs_working_dir):
+        return f'Error: "{directory}" is not a directory'
+
+    final_response = ""
+
+    contents = os.listdir(abs_directory)
+    for content in contents:
+        content_path = os.path.join(abs_directory, content)
+        is_dir = os.path.isdir(content_path)
+        size = os.path.getsize(content_path)
+        final_response += f"- {content}: file_size={size} bytes, is_dir={is_dir}\n"
+
+    return final_response
