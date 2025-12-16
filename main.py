af708c16904b1f307635f71aef7dc8019ee23385
yoshiioko <acardoza@pdx.edu>
2025-12-15 16:18:30 -0800

    Initial commit for project

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001", contents="Why is the sky blue?"
+)
+
+print(response.text)
