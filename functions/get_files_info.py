e5751c04cdb4a6d260e16b7d572cae596d319c3e
yoshiioko <acardoza@pdx.edu>
2025-12-16 20:33:11 -0800

    Create a schema for the function for the agent to use.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 
 def get_files_info(working_directory, directory="."):
@@ -18,3 +19,18 @@ def get_files_info(working_directory, directory="."):
         final_response += f"- {content}: file_size={size} bytes, is_dir={is_dir}\n"
 
     return final_response
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
