f47656392735661bf13b4bd7db478231c2d15f78
yoshiioko <acardoza@pdx.edu>
2025-12-16 20:57:28 -0800

    Update sys prompt. Include rest of function definitions.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
+from functions.write_file import schema_write_file
 
 
 def main():
@@ -17,6 +20,9 @@ You are a helpful AI coding agent.
 When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
 - List files and directories
+- Read the content of a file
+- Write to a file (create or update)
+- Run a Python file with optional arguments
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
@@ -36,7 +42,12 @@ All paths you provide should be relative to the working directory. You do not ne
     ]
 
     available_functions = types.Tool(
-        function_declarations=[schema_get_files_info]
+        function_declarations=[
+            schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file
+        ]
     )
 
     config = types.GenerateContentConfig(
